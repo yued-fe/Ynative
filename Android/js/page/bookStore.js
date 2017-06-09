599bc19f269eb307dc7b84d2956581d263285a88
p_kunni <nikun@yuewen.com>
2017-06-09 14:49:06 +0800

    书城deving

--- Android/js/page/bookStore.js
+++ Android/js/page/bookStore.js
@@ -6,7 +6,7 @@ import {bindActionCreators} from 'redux';
 import * as Actions from '../actions/requestIndexData';
 import {StyleSheet, View, Text,Dimensions,ActivityIndicator, Image, ScrollView} from 'react-native';
 import Swiper from 'react-native-swiper';
-const Slide = props => {
+/*const Slide = props => {
   return (<View style={styles.slide}>
     <Image onLoad={props.loadHandle.bind(null, props.i)} style={styles.image} source={{uri: props.uri}} />
     {
@@ -15,7 +15,8 @@ const Slide = props => {
       </View>
     }
   </View>)
-}
+}*/
+
 
 class BookStorePage extends Component{
     constructor(props){
@@ -42,35 +43,30 @@ class BookStorePage extends Component{
     render(){
         return(
             <View style={styles.container}>
-                <Swiper loadMinimal loadMinimalSize={1} style={styles.wrapper} height={240} loop={true} autoplay={true}>
+                <Swiper style={styles.wrapper} width={'100%'} height={250} loop={true} autoplay={true}>
                        <Image  style={styles.image} source={{uri: 'https://qidian.qpic.cn/qidian_common/349573/ad932201175a77c7f96ed28d0c3f1acf/0'}} />
                        <Image  style={styles.image} source={{uri: 'https://qidian.qpic.cn/qidian_common/349573/ca9c6ea4e5d70f2542e4a0791e82b3cb/0'}} />
                        <Image  style={styles.image} source={{uri: 'https://qidian.qpic.cn/qidian_common/349573/cab2778493c43e33237adff16b62308e/0'}} />
                        <Image  style={styles.image} source={{uri: 'https://qidian.qpic.cn/qidian_common/349573/ad932201175a77c7f96ed28d0c3f1acf/0'}} />                       
                 </Swiper>    
                 <View style={styles.nav}>
-                    <View style={styles.iconBox}><Image source={require('../res/rank.png')} /><Text style={styles.navText}>排行榜</Text></View> 
-                    <View style={styles.iconBox}><Image source={require('../res/free.png')} /><Text style={styles.navText}>新书</Text></View> 
-                    <View style={styles.iconBox}><Image source={require('../res/sort.png')} /><Text style={styles.navText}>完本</Text></View> 
-                    <View style={styles.iconBox}><Image source={require('../res/end.png')} /><Text style={styles.navText}>分类</Text></View>                
-                </View>      
+                    <View style={styles.iconBox}><Image style={styles.iconImg} width={38} height={38} source={require('../res/rank.png')} /><Text style={styles.navText}>排行榜</Text></View> 
+                    <View style={styles.iconBox}><Image style={styles.iconImg} width={38} height={38}  source={require('../res/free.png')} /><Text style={styles.navText}>新书</Text></View> 
+                    <View style={styles.iconBox}><Image style={styles.iconImg} width={32} height={40}  source={require('../res/sort.png')} /><Text style={styles.navText}>完本</Text></View> 
+                    <View style={styles.iconBox}><Image style={styles.iconImg} width={32} height={40}  source={require('../res/end.png')} /><Text style={styles.navText}>分类</Text></View>                
+                </View>  
+                <Text style={styles.title}>热门小说</Text>    
                 <ScrollView horizontal={true} style={styles.bookList}>
-                    <View style={styles.iconBox}><Image source={require('../res/rank.png')} /><Text style={styles.navText}>排行榜</Text></View> 
-                    <View style={styles.iconBox}><Image source={require('../res/free.png')} /><Text style={styles.navText}>新书</Text></View> 
-                    <View style={styles.iconBox}><Image source={require('../res/sort.png')} /><Text style={styles.navText}>完本</Text></View> 
-                    <View style={styles.iconBox}><Image source={require('../res/end.png')} /><Text style={styles.navText}>分类</Text></View>
-                    <View style={styles.iconBox}><Image source={require('../res/rank.png')} /><Text style={styles.navText}>排行榜</Text></View> 
-                    <View style={styles.iconBox}><Image source={require('../res/free.png')} /><Text style={styles.navText}>新书</Text></View> 
-                    <View style={styles.iconBox}><Image source={require('../res/sort.png')} /><Text style={styles.navText}>完本</Text></View> 
-                    <View style={styles.iconBox}><Image source={require('../res/end.png')} /><Text style={styles.navText}>分类</Text></View>
-                    <View style={styles.iconBox}><Image source={require('../res/end.png')} /><Text style={styles.navText}>分类</Text></View>
-                    <View style={styles.iconBox}><Image source={require('../res/end.png')} /><Text style={styles.navText}>分类</Text></View>
-                    <View style={styles.iconBox}><Image source={require('../res/end.png')} /><Text style={styles.navText}>分类</Text></View>
-                    <View style={styles.iconBox}><Image source={require('../res/end.png')} /><Text style={styles.navText}>分类</Text></View>
-                    <View style={styles.iconBox}><Image source={require('../res/end.png')} /><Text style={styles.navText}>分类</Text></View>
-                    <View style={styles.iconBox}><Image source={require('../res/end.png')} /><Text style={styles.navText}>分类</Text></View>
-                    
-
+                    <BookListItem bookCover='https://qidian.qpic.cn/qdbimg/349573/c_5282978903343101/150' bookName='婚途有坑' authorName='豆丁丁'></BookListItem>
+                    <BookListItem bookCover='https://qidian.qpic.cn/qdbimg/349573/c_5282978903343101/150' bookName='婚途有坑' authorName='豆丁丁'></BookListItem>
+                    <BookListItem bookCover='https://qidian.qpic.cn/qdbimg/349573/c_5282978903343101/150' bookName='婚途有坑' authorName='豆丁丁'></BookListItem>
+                    <BookListItem bookCover='https://qidian.qpic.cn/qdbimg/349573/c_5282978903343101/150' bookName='婚途有坑' authorName='豆丁丁'></BookListItem>
+                    <BookListItem bookCover='https://qidian.qpic.cn/qdbimg/349573/c_5282978903343101/150' bookName='婚途有坑' authorName='豆丁丁'></BookListItem>
+                    <BookListItem bookCover='https://qidian.qpic.cn/qdbimg/349573/c_5282978903343101/150' bookName='婚途有坑' authorName='豆丁丁'></BookListItem>
+                    <BookListItem bookCover='https://qidian.qpic.cn/qdbimg/349573/c_5282978903343101/150' bookName='婚途有坑' authorName='豆丁丁'></BookListItem>
+                    <BookListItem bookCover='https://qidian.qpic.cn/qdbimg/349573/c_5282978903343101/150' bookName='婚途有坑' authorName='豆丁丁'></BookListItem>
+                    <BookListItem bookCover='https://qidian.qpic.cn/qdbimg/349573/c_5282978903343101/150' bookName='婚途有坑' authorName='豆丁丁'></BookListItem>
+                    <BookListItem bookCover='https://qidian.qpic.cn/qdbimg/349573/c_5282978903343101/150' bookName='婚途有坑' authorName='豆丁丁'></BookListItem>
                 </ScrollView>     
             </View>
         );
@@ -85,8 +81,20 @@ class BookStorePage extends Component{
                 store.dispatch(fetchData());
         */
     }
-
-
+}
+class BookListItem extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <View style={styles.bookListItem}>
+                <Image style={styles.bookCover} source={{uri: 'https://qidian.qpic.cn/qdbimg/349573/c_5282978903343101/150'}} />
+                <Text>{this.props.bookName}</Text>
+                <Text>{this.props.authorName}</Text>                
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -99,27 +107,46 @@ const styles = StyleSheet.create({
     },
 
     slide: {
+        width: '100%',
         flex: 1,
         justifyContent: 'center',
     },
     image: {
-        width,
-        flex: 1,
-        backgroundColor: 'transparent'
+        backgroundColor: 'transparent',
+        width: '100%',
+        height: 250
     },
     nav: {
         height: 100,                
         flex: 1,
         flexDirection:'row',
         justifyContent: 'space-around',
+        marginTop: 10,
+        marginBottom: 10
     },
     iconBox: {
         flex: 1,
-        height: 100,
-                                
+        alignItems: 'center',        
+        height: 100
+    },
+    iconImg: {
+    },
+    bookCover: {
+        flex: 1,
+        width: 100,
+        height: 90
     },
     navText: {
         color: '#333',
+    },
+    bookListItem: { 
+        flex: 1,
+        paddingTop: 15,
+        paddingLeft: 10,
+        paddingRight: 10, 
+        paddingBottom: 10
+    },
+    title: {    
     },
     bookList: {
         height: 50,

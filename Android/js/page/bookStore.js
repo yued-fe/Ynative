937960f8b4bbfeb549c7556130da3fabdeeb1d87
周文康 <zhouwenkang@zhouwenkangdeiMac.local>
2017-06-09 15:59:55 +0800

    增加首页跳转

--- Android/js/page/bookStore.js
+++ Android/js/page/bookStore.js
@@ -1,59 +1,29 @@
 'use strict';
 
 import React, {Component, PropTypes} from 'react';
-import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux';
-import * as Actions from '../actions/requestIndexData';
 import {StyleSheet, View, Text,Dimensions,ActivityIndicator, Image, ScrollView} from 'react-native';
 import Swiper from 'react-native-swiper';
-/*const Slide = props => {
-  return (<View style={styles.slide}>
-    <Image onLoad={props.loadHandle.bind(null, props.i)} style={styles.image} source={{uri: props.uri}} />
-    {
-      !props.loaded && <View style={styles.loadingView}>
-        
-      </View>
-    }
-  </View>)
-}*/
+import CategoryPage from './category';
 
 
 class BookStorePage extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            imgList: [
-                'https://qidian.qpic.cn/qidian_common/349573/ad932201175a77c7f96ed28d0c3f1acf/0',
-                'https://qidian.qpic.cn/qidian_common/349573/ca9c6ea4e5d70f2542e4a0791e82b3cb/0',
-                'https://qidian.qpic.cn/qidian_common/349573/cab2778493c43e33237adff16b62308e/0',
-                'https://qidian.qpic.cn/qidian_common/349573/ad932201175a77c7f96ed28d0c3f1acf/0'
-            ],
-            loadQueue: [0, 0, 0, 0]
-        };
-        // this.loadHandle = this.loadHandle.bind(this);
-    }
-
-    loadHandle (i) {
-        let loadQueue = this.state.loadQueue;
-        loadQueue[i] = 1;
-        this.setState({
-            loadQueue
-        })
     }
     render(){
         return(
             <View style={styles.container}>
                 <Swiper style={styles.wrapper} width={'100%'} height={250} loop={true} autoplay={true}>
-                       <Image  style={styles.image} source={{uri: 'https://qidian.qpic.cn/qidian_common/349573/ad932201175a77c7f96ed28d0c3f1acf/0'}} />
-                       <Image  style={styles.image} source={{uri: 'https://qidian.qpic.cn/qidian_common/349573/ca9c6ea4e5d70f2542e4a0791e82b3cb/0'}} />
-                       <Image  style={styles.image} source={{uri: 'https://qidian.qpic.cn/qidian_common/349573/cab2778493c43e33237adff16b62308e/0'}} />
-                       <Image  style={styles.image} source={{uri: 'https://qidian.qpic.cn/qidian_common/349573/ad932201175a77c7f96ed28d0c3f1acf/0'}} />                       
+                    <Image  style={styles.image} source={{uri: 'https://qidian.qpic.cn/qidian_common/349573/ad932201175a77c7f96ed28d0c3f1acf/0'}} />
+                    <Image  style={styles.image} source={{uri: 'https://qidian.qpic.cn/qidian_common/349573/ca9c6ea4e5d70f2542e4a0791e82b3cb/0'}} />
+                    <Image  style={styles.image} source={{uri: 'https://qidian.qpic.cn/qidian_common/349573/cab2778493c43e33237adff16b62308e/0'}} />
+                    <Image  style={styles.image} source={{uri: 'https://qidian.qpic.cn/qidian_common/349573/ad932201175a77c7f96ed28d0c3f1acf/0'}} />                       
                 </Swiper>    
                 <View style={styles.nav}>
-                    <View style={styles.iconBox}><Image style={styles.iconImg} width={38} height={38} source={require('../res/rank.png')} /><Text style={styles.navText}>排行榜</Text></View> 
+                    <View style={styles.iconBox}><Image style={styles.iconImg} width={38} height={38}  source={require('../res/rank.png')} /><Text style={styles.navText}>排行榜</Text></View> 
                     <View style={styles.iconBox}><Image style={styles.iconImg} width={38} height={38}  source={require('../res/free.png')} /><Text style={styles.navText}>新书</Text></View> 
                     <View style={styles.iconBox}><Image style={styles.iconImg} width={32} height={40}  source={require('../res/sort.png')} /><Text style={styles.navText}>完本</Text></View> 
-                    <View style={styles.iconBox}><Image style={styles.iconImg} width={32} height={40}  source={require('../res/end.png')} /><Text style={styles.navText}>分类</Text></View>                
+                    <View style={styles.iconBox}><Image style={styles.iconImg} width={32} height={40}  source={require('../res/end.png')} /><Text style={styles.navText} onPress={() => this.goCategoryPage()}>分类</Text></View>                
                 </View>  
                 <View style={styles.title}><Text style={styles.titleText}>热门小说</Text></View>    
                 <ScrollView horizontal={true} style={styles.bookList}>
@@ -72,14 +42,14 @@ class BookStorePage extends Component{
         );
     }
 
-    loadData () {
-        //设置了mapDispatchToProps可以这么调用
-        this.props.actions.fetchData();
-        /*
-            没设置mapDispatchToProps可以这么调用：
-                import {fetchData} from '../actions/requestIndexData';
-                store.dispatch(fetchData());
-        */
+    goCategoryPage () {
+        this.switchPage(CategoryPage);
+    }
+
+    switchPage(component){
+        this.props.navigator.push({
+            component: component
+        });
     }
 }
 class BookListItem extends Component {

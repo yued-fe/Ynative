8e6cf806b3f4737c4fff5ae9343d446bbd24c595
p_wkangzhou <koudaix700@gmail.com>
2017-06-20 18:07:03 +0800

    增加离线bundle支持

--- IOS/ios/RNReadnovel/AppDelegate.m
+++ IOS/ios/RNReadnovel/AppDelegate.m
@@ -17,13 +17,15 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-
+  
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
+  //jsCodeLocation = [NSURL URLWithString:[[NSBundle mainBundle] pathForResource:@"index.ios.jsbundle" ofType:nil]];
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"RNReadnovel"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
+                                                      initialProperties:nil
+                                                      launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
